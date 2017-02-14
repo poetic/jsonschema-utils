09380823827ac02d334b5f629f83a4ab7073fdc8
Chun-Yang <yang2007chun@gmail.com>
2017-02-14 17:42:24 -0600

    flattenJsonSchema

---
+++ index.js
@@ -0,0 +1,40 @@
+function combineProperties (properties, parentPath) {
+  const flattenedJsonSchemas = Object
+    .keys(properties)
+    .map((propertyKey) => {
+      return flattenJsonSchema(
+        properties[propertyKey],
+        parentPath ? [parentPath, propertyKey].join('.'): propertyKey
+      );
+    });
+
+  return Array.prototype.concat.apply(
+    Array.prototype,
+    flattenedJsonSchemas
+  );
+}
+
+function flattenJsonSchema(jsonSchema, parentPath) {
+  const type = jsonSchema.type;
+
+  switch (type) {
+    case 'object':
+      return combineProperties(
+        jsonSchema.properties,
+        parentPath
+      );
+
+    case 'array':
+      return flattenJsonSchema(
+        jsonSchema.items,
+        parentPath ? [parentPath, '[]'].join('.'): '[]'
+      );
+
+    default:
+      return [{ path: parentPath, type: type }];
+  }
+}
+
+module.exports = {
+  flattenJsonSchema
+}
